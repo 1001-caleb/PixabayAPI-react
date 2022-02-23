2ce10ab70a44cd15728550e879ab9974f3a8f750
1001-caleb <mccalebyt@gmail.com>
2022-02-23 10:01:13 -0500

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
+import { Component } from 'react/cjs/react.development'
 import './App.css'
 import Buscador from './components/Buscador'
 
-function App() {
-  return (
-    <div className="container">
-      <h1 className='text-center'>Pixabay API</h1>
-      <div className="jumbotron">
-       <p className="lead text-center">Buscador de imagenes</p>
-      </div>
+class App extends Component {
 
-      <Buscador />
-    </div>
-  )
-}
+  datosBusqueda = (termino)=>{
+    console.log(termino);
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h1 className='text-center'>Pixabay API</h1>
+        <div className="jumbotron">
+         <p className="lead text-center">Buscador de imagenes</p>
+        </div>
+  
+        <Buscador 
+          datosBusqueda={this.datosBusqueda}
+        />
+      </div>
+    )
+  }
+  }
+
 
 export default App
--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,12 +1,29 @@
+import react from 'react';
 import React, { Component } from 'react'
 
 class Buscador extends Component{
+
+    busquedaRef = react.createRef();
+    obtenerDatos = (e) =>{
+        e.preventDefault();
+
+        
+        const termino = this.busquedaRef.current.value;
+
+        this.props.datosBusqueda(termino)
+    }
     render(){
         return (
-            <form>
-                <div class="row">
+            <form onSubmit={this.obtenerDatos}>
+                <div className="row">
+                    
                     <div className='form-group col-md-8'>
-                        <input type="text" className='form-control form-control-lg' placeholder='Busca tu img. Ejemplo: Fusbol'/>
+                        <input ref={this.busquedaRef} type="text" className='form-control form-control-lg' placeholder='Busca tu img. Ejemplo: Fusbol'/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className='form-group col-md-4'>
+                        <input type="submit" className='btn' value="Buscar"/>
                     </div>
                 </div>
             </form>
