03a7aa39a9772d78b2afc4ce5d141b4da1701b2d
1001-caleb <mccalebyt@gmail.com>
2022-02-23 11:43:15 -0500

    added result

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import { Component } from 'react/cjs/react.development'
 import './App.css'
 import Buscador from './components/Buscador'
+import Result  from './components/result'
 
 class App extends Component {
 
   state ={
-    termino: ""
+    termino: "",
+    imagenes: []
   }
 
   consultarApi = () =>{
-    const url = `https://pixabay.com/api/?key=25849559-be94efa9db328b81fed769120&q=${this.state.termino}`; 
-    console.log(url);
+    const termino = this.state.termino
+    const url = `https://pixabay.com/api/?key=25849559-be94efa9db328b81fed769120&q=${termino}&per_page=30`; 
+
+    // console.log(url);
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => this.setState({imagenes: resultado.hits}))
   }
 
   datosBusqueda = (termino)=>{
@@ -33,7 +41,9 @@ class App extends Component {
           datosBusqueda={this.datosBusqueda}
         />
         </div>
-        {this.state.termino}
+        <Result 
+          imagenes ={this.state.imagenes}
+        />
       </div>
       
     )
